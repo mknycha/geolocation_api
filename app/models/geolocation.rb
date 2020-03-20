cecfcb5d1bf612a7af70f42c72cae89823109f4d
Marcin K <marcin.knychala@bitgamelabs.com>
2020-03-20 19:56:20 +0100

    Geolocation model: add ip validation

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -2,6 +2,18 @@
 
 class Geolocation < ApplicationRecord
   validates :ip, :ip_type, :country, :city, :longitude, :latitude, presence: true
+  validates :ip_type, inclusion: { in: %w[ipv4 ipv6] }
   validates :ip, uniqueness: true
-  # TODO: Add ip validation
+  validate :ip_address_must_be_valid
+
+  def ip_address_must_be_valid
+    return if ip_valid?(ip)
+
+    errors.add(:ip, 'is invalid')
+  end
+
+  def ip_valid?(ip)
+    (ip_type == 'ipv4' && IPAddress.valid_ipv4?(ip)) ||
+      (ip_type == 'ipv6' && IPAddress.valid_ipv6?(ip))
+  end
 end
